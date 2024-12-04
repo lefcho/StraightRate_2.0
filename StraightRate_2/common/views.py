9474b23c14068474a0f0402bfca34b238fa7aba2
ivand <ivan.d.kostov@gmail.com>
2024-12-04 19:41:46 +0200

    fixed: main page shows only approved media

--- StraightRate_2/common/views.py
+++ StraightRate_2/common/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import TemplateView
 from django.db.models import Avg, Count
-
 from StraightRate_2.media.models import Movie, VideoGame
 
 
@@ -11,7 +10,7 @@ class HomeView(TemplateView):
     def get_top_rated_items(model, limit=5):
         return (model.objects
                 .annotate(avg_rating=Avg('reviews__rating'), review_count=Count('reviews'))
-                .filter(review_count__gt=0)
+                .filter(review_count__gt=0, approved=True)
                 .order_by('-avg_rating')[:limit])
 
     def get_context_data(self, **kwargs):
