aea747fb2f539c942cc9313a949d3625c3151e18
ivand <ivan.d.kostov@gmail.com>
2024-12-06 18:50:29 +0200

    fixed: added security, so you are not able to suggest creator without permission

--- StraightRate_2/creators/views.py
+++ StraightRate_2/creators/views.py
@@ -1,19 +1,28 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from StraightRate_2.creators.forms import DirectorCreateForm, DeveloperCreateForm
 from StraightRate_2.creators.models import Director, Developer
 
 
-class AddDirectorView(LoginRequiredMixin, CreateView):
+class AddDirectorView(PermissionRequiredMixin, CreateView):
     model = Director
     form_class = DirectorCreateForm
     template_name = 'directors/add-director.html'
     success_url = reverse_lazy('suggest-movie')
+    permission_required = 'media.can_suggest_movies'
+
+    def handle_no_permission(self):
+        return redirect('home')
 
 
-class AddDeveloperView(LoginRequiredMixin, CreateView):
+class AddDeveloperView(PermissionRequiredMixin, CreateView):
     model = Developer
     form_class = DeveloperCreateForm
     template_name = 'developers/add-developer.html'
     success_url = reverse_lazy('suggest-game')
+    permission_required = 'media.can_suggest_games'
+
+    def handle_no_permission(self):
+        return redirect('home')
