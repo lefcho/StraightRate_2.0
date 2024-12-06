eaa941aa3d334666809511a834e1adba0e3db466
ivand <ivan.d.kostov@gmail.com>
2024-12-06 20:09:48 +0200

    added: search bar functionality

--- StraightRate_2/common/urls.py
+++ StraightRate_2/common/urls.py
@@ -4,4 +4,5 @@ from StraightRate_2.common import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('search/', views.SearchedMediaView.as_view(), name='search'),
 ]
--- StraightRate_2/common/views.py
+++ StraightRate_2/common/views.py
@@ -1,5 +1,8 @@
+from django.db.models.functions import Concat
 from django.views.generic import TemplateView
-from django.db.models import Avg, Count
+from django.db.models import Avg, Count, Value, Q
+
+from StraightRate_2.creators.models import Developer, Director
 from StraightRate_2.media.models import Movie, VideoGame
 
 
@@ -17,4 +20,35 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['top_movies'] = self.get_top_rated_items(Movie)
         context['top_games'] = self.get_top_rated_items(VideoGame)
+        return context
+
+
+class SearchedMediaView(TemplateView):
+    template_name = 'common/search.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('query', '')
+
+        directors = Director.objects.annotate(
+            full_name=Concat('first_name', Value(' '), 'last_name')
+        ).filter(Q(full_name__icontains=query))
+
+        developers = Developer.objects.filter(developer_name__icontains=query)
+
+        if len(query) == 1:
+            movie_results = Movie.objects.filter(title__istartswith=query)
+            game_results = VideoGame.objects.filter(title__istartswith=query)
+        else:
+            movie_results = Movie.objects.filter(title__icontains=query)
+            game_results = VideoGame.objects.filter(title__icontains=query)
+
+        context.update({
+            'query': query,
+            'movie_results': movie_results,
+            'game_results': game_results,
+            'directors': directors,
+            'developers': developers,
+        })
+
         return context
