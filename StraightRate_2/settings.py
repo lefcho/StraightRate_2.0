e8c6a89be510eba1dbfd01f569941355eaaae528
ivand <ivan.d.kostov@gmail.com>
2024-12-12 19:35:38 +0200

    CSRF TRUSTED ADDED

--- StraightRate_2/settings.py
+++ StraightRate_2/settings.py
@@ -18,7 +18,7 @@ DEBUG = os.getenv('DEBUG', config('DEBUG', default=False, cast=bool)) == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',
                           config('ALLOWED_HOSTS', default='')).split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', '')).split(',')
 
 # Application definition
 
