3467fe73809507f1594d52b3528b31f7e0f56d23
ivand <ivan.d.kostov@gmail.com>
2024-12-12 19:26:29 +0200

    collect static

--- StraightRate_2/settings.py
+++ StraightRate_2/settings.py
@@ -18,6 +18,8 @@ DEBUG = os.getenv('DEBUG', config('DEBUG', default=False, cast=bool)) == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',
                           config('ALLOWED_HOSTS', default='')).split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
+
 # Application definition
 
 STRAIGHT_RATE_APPS = [
